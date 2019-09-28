a0847b072006e2e566722af32393169a1d5dcc0a
MoeinZ <moein459@gmail.com>
2019-09-28 23:29:03 +0330

    comment unnecessary postProcess method ~ - fix README files based on that

--- projects/neo-crumb/README.md
+++ projects/neo-crumb/README.md
@@ -60,8 +60,9 @@ Also for changing values at runtime based on app logic, you can use NeoCrumbServ
 ```javascript
 constructor(private neoCrumbService: NeoCrumbService) {
     this.neoCrumbService.change$.subscribe(value => {
-        value.map(nc => nc.breadcrumb += ' test');
-        this.neoCrumbService.postProcess(value);
+        value.forEach(nc => nc.breadcrumb += ' test');
     })
+    
+    // this.neoCrumbService.postProcess([]);
 }
 ```
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,14 +9,16 @@ import {NeoCrumbService} from 'ngx-neocrumb';
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 	subscription = new Subscription();
+
 	constructor(private neoCrumbService: NeoCrumbService) {
 	}
 
 	ngOnInit(): void {
 		this.subscription = this.neoCrumbService.change$.subscribe(value => {
-			value.map(nc => nc.breadcrumb += ' test');
-			this.neoCrumbService.postProcess(value);
+			value.forEach(nc => nc.breadcrumb += ' test');
 		});
+
+		// this.neoCrumbService.postProcess([]);
 	}
 
 	ngOnDestroy(): void {
